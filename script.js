e0cb234232bdbfa59edb572b0ca425361f3d50ba
matheushmacedo <matheus.hmacedo@hotmail.com>
2021-06-05 10:29:45 -0300

    fazendo transicao de opacidade ao clicar no link gerando efeito no modal

--- script.js
+++ script.js
@@ -9,10 +9,17 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description; // add a descrição do item
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`; // add o preço do item e fixa em 2 casas decimais após o ponto
 
+    // fica escutando pra ver se houve o click
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
-        e.preventDefault();
-        console.log('Clicou na pizza')
-    })
+        e.preventDefault(); // previne o comportamento padrao do click
+
+        selector('.pizzaWindowArea').style.opacity = 0;
+        selector('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(()=>{
+            selector('.pizzaWindowArea').style.opacity = 1;
+        }, 200);
+        
+    });
 
 
     selector('.pizza-area').append(pizzaItem);

0e716c6740be82ccdabf625ae19f6a0720e62724
matheushmacedo <matheus.hmacedo@hotmail.com>
2021-06-05 10:08:16 -0300

    Add o preco e fixando em 2 casas decimais apos o ponto

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const selectorAll = (elemento) => document.querySelector(elemento);
 pizzaJson.map((item, index) => {
     let pizzaItem = selector('.models .pizza-item').cloneNode(true);
 
-    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name; // add nome do item
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description; // add a descrição do item
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`; // add o preço do item e fixa em 2 casas decimais após o ponto
 
 
     selector('.pizza-area').append(pizzaItem);

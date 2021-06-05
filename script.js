bec0fac15c79d81c18639d3b7aa47aaffdb6109a
matheushmacedo <matheus.hmacedo@hotmail.com>
2021-06-05 10:10:50 -0300

    Add a imagem do item

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const selectorAll = (elemento) => document.querySelector(elemento);
 pizzaJson.map((item, index) => {
     let pizzaItem = selector('.models .pizza-item').cloneNode(true);
 
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img; // add a imagem do item
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name; // add nome do item
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description; // add a descrição do item
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`; // add o preço do item e fixa em 2 casas decimais após o ponto
